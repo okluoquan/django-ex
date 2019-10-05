cb9fb2f9d2c8f12d068547880f8e71c9e1a36e05
okluoquan <okluoquan@163.com>
2019-10-05 11:09:36 +0800

    修改

--- project/websockets.py
+++ project/websockets.py
@@ -43,15 +43,15 @@ from bsv_thisweek_okex.models import Trade as BsvTradeThisweek
 
 wsurl = "wss://149.129.81.70:10442/ws/v3"
 instrument_ids = [
-	"futures/trade:BTC-USD-191004",
-	"futures/trade:LTC-USD-191004",
-	"futures/trade:ETH-USD-191004",
-	"futures/trade:ETC-USD-191004",
-	"futures/trade:XRP-USD-191004",
-	"futures/trade:EOS-USD-191004",
-	"futures/trade:TRX-USD-191004",
-	"futures/trade:BCH-USD-191004",
-	"futures/trade:BSV-USD-191004",
+	"futures/trade:BTC-USD-191011",
+	"futures/trade:LTC-USD-191011",
+	"futures/trade:ETH-USD-191011",
+	"futures/trade:ETC-USD-191011",
+	"futures/trade:XRP-USD-191011",
+	"futures/trade:EOS-USD-191011",
+	"futures/trade:TRX-USD-191011",
+	"futures/trade:BCH-USD-191011",
+	"futures/trade:BSV-USD-191011",
 ]
 
 modify_instrument_ids = False
